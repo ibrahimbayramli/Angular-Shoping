fe4f3aa4cab99a7b59093747e08d56aa1486c12f
ibrahimbayramli <54009509+ibrahimbayramli@users.noreply.github.com>
2022-12-01 00:16:54 +0300

    project created

---
+++ shopping/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'shopping';
+  constructor(private _toastr: ToastrService) {
+    this._toastr.success(("Deneme mesaj")); //çalıştı çok teşekkürler hocamkolay gelsin
+  }
+}
